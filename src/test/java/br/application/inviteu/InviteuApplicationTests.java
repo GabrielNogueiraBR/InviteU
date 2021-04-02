8def92e04450178a437b618ebb061f5f22aaa13c
Gabriel Nogueira <gabrielnoog7@gmail.com>
2021-04-02 12:27:55 -0300

    Criação da estrutura do projeto utilizando SpringBoot

---
+++ src/test/java/br/application/inviteu/InviteuApplicationTests.java
@@ -0,0 +1,13 @@
+package br.application.inviteu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InviteuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
