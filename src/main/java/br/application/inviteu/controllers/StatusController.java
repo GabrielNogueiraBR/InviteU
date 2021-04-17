068ebd2b3cbfd13e3f20fa072160deff53055e73
Gabriel Nogueira <gabrielnoog7@gmail.com>
2021-04-17 10:41:49 -0300

    Add endpoint getStatusById #5.1

--- src/main/java/br/application/inviteu/controllers/StatusController.java
+++ src/main/java/br/application/inviteu/controllers/StatusController.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.application.inviteu.entities.Status;
 import br.application.inviteu.services.StatusService;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/status")
@@ -23,4 +26,11 @@ public class StatusController {
         List<Status> list = service.getAllStatus();
         return ResponseEntity.ok(list);
     }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Status> getStatusById(@PathVariable Long id) {
+        Status status = service.getStatusById(id);
+        return ResponseEntity.ok(status);
+    }
+    
 }
--- src/main/java/br/application/inviteu/services/StatusService.java
+++ src/main/java/br/application/inviteu/services/StatusService.java
@@ -1,9 +1,12 @@
 package br.application.inviteu.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.application.inviteu.entities.Status;
 import br.application.inviteu.repositories.StatusRepository;
@@ -16,6 +19,11 @@ public class StatusService {
 
     public List<Status> getAllStatus() {
         return repository.findAll();
+    }
+
+    public Status getStatusById(Long id) {
+        Optional<Status> op = repository.findById(id);
+        return op.orElseThrow( ()-> new ResponseStatusException(HttpStatus.NOT_FOUND, "Status not found"));    
     }
     
 }
